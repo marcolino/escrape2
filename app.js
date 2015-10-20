78d4f438eb4a4ab4a70068bf16954ca4fb01a2b7
marcolino <marcosolari@gmail.com>
2015-10-20 08:30:12 +0200

    started client side

---
+++ app.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var express = require('express') // web server
+  , logger = require('morgan') // clients requests logger
+  , bodyParser = require('body-parser') // body parser
+  , methodOverride = require('method-override') // method overryde for clients not supporting all REST verbs
+  , config = require('./config') // application configuration
+  , db = require('./models/db') // database wiring
+;
+
+// setup
+var pathStatic = __dirname + '/public'; // path to static directory
+var pathViews = __dirname + '/views'; // path to views directory
+var engineTemplate = 'jade'; // template engine name
+
+// required routes
+//var main = require('./routes/main');
+var providers = require('./routes/providers');
+var users = require('./routes/users');
+var persons = require('./routes/persons');
+var places = require('./routes/places');
+var comments = require('./routes/comments');
+
+// create express application
+var app = module.exports = express();
+
+// use modules to become RESTful
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// routes redirection
+//////////////////////app.use('/', main);
+app.use('/users', users);
+app.use('/persons', persons);
+app.use('/providers', providers);
+app.use('/places', places);
+app.use('/comments', comments);
+app.use(express.static(pathStatic));
+app.set('views', pathViews);
+app.set('view engine', engineTemplate); 
+
+// error handlers
+
+app.use(function(req, res, next) {
+  var status = 404;
+  res.status(status);
+  var err = new Error();
+  err.message = 'not found';
+  err.status = status;
+  if (config.env === 'development') {
+    err.stacktrace = err.stack;
+  }
+  res.send({ error: err });
+});
+
+// error-handling middleware
+app.use(function(err, req, res, next) {
+  var err = new Error('not allowed');
+  res.status(err.status || 500);
+  res.send({ error: err });
+});
