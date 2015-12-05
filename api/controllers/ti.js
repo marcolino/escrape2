a18d456c5557148644da2830ad31811921e2707f
marcolino <marcosolari@gmail.com>
2015-12-05 15:05:57 +0100

    built a working schematic syncPersonsImages() in ti.js

--- api/controllers/ti.js
+++ api/controllers/ti.js
@@ -1,5 +1,6 @@
     var config = require('../config');
     var async = require('async');
+    var crypto = require('crypto');
     var request = require('request');
 
     var log = config.log;
@@ -22,8 +23,9 @@
                       ], },
     ];
     var images = [
+      { personKey: 'P2/K1', url: 'http://dummyimage.com/450x450/0f0/fff', }
     ];
-    
+
     var syncPersonsImages = function(persons, images, callback) {
       async.each(
         persons,
@@ -32,70 +34,78 @@
             person.imageUrls,
             function(imageUrl, callbackImage) {
               download(imageUrl, function(err, image) {
-                if (err) return err;
-                downloadPost(callbackImage);
+                check(image, callbackImage);
               });
             },
             function(err) {
-              if (err) return err;
               callbackPerson();
             }
           );
         },
         function(err) {
-          if (err) return err;
-          callback(persons);
+          callback(null, persons);
         }
       );
-    
+
       function download(imageUrl, callback) {
         request(
-          { uri: imageUrl, },
-          function(err) {
-            callback(err);
-          },
-          function(contents, res) {
-            image.contents = contents;
-            return callback(null, image);
+          { uri: imageUrl },
+          function (err, response, contents) {
+            if (!err && response.statusCode === 200) {
+              var image = {}; // new Image() omitted for simplicity
+              image.url = response.request.uri.href;
+              image.contents = contents;
+              callback(null, image);              
+            } else {
+              log.error('err in image download:', err, response.statusCode);              
+            }
           }
         );
       }
 
-      function downloadPost(image, callback) {
+      function check(image, callback) {
         async.waterfall(
           [
-            getSignatureFromImage,
+            function(callback) { getSignatureFromImage(image, callback); },
             findSimilarSignatureImage,
             saveImage,
           ],
           function (err, image) {
-            callback(image);
+            callback();
           }
         );
       }
     
-      function getSignatureFromImage(image, getSignatureFromImageCallback) {
-        // ...
-        getSignatureFromImageCallback(null);
+      var getSignatureFromImage = function(image, callback) {
+        var image = {}; image.url = 'http://dummyimage.com/250x250/f00/fff'; // TODO: how to pass parameters to first waterfall function?
+        image.signature = crypto.createHash('md5').update('image.url').digest('hex');
+        callback(null, image);
       }
     
-      function findSimilarSignatureImage(image, findSimilarSignatureImageCallback) {
-        // ...
-        findSimilarSignatureImageCallback(null, image);
+      var findSimilarSignatureImage = function(image, callback) {
+        if (existsAlready(image.signature)) {
+          image.isNew = true;
+        }
+        callback(null, image);
       }
     
-      function saveImage(image, saveCallback) {
-        // ...
-        saveCallback(null, image);
+      var saveImage = function(image, callback) {
+        if (image.isNew) {
+          // save image to db/fs omitted for simplicity
+          console.log('image', image.url, 'saved');
+          callback(null, image);
+        } else {
+          console.log('image', image.url, 'not saved');
+          callback(null, null);
+        }   
       }
     
+      function existsAlready(signature) {
+        return Math.random() < 0.5; // random value, for simplicity
+      }
+
     };
 
     syncPersonsImages(persons, images, function(err, persons) {
-      if (err) {
-        log.error(err);
-        return err;
-      }
-      log.info('result:', persons);
+      log.info('done syncy\'ing persons images');
     });
-
